6880f4f833f5344757b5a29bc6f2eb41434c486d
Wolfgang <wleon@bakrypt.io>
2023-12-26 21:48:45 -0500

    chore: utils functions

--- src/components/composites/Asset/AssetForm.tsx
+++ src/components/composites/Asset/AssetForm.tsx
@@ -2,33 +2,7 @@ import React, { useState } from 'react';
 import { Divider, Form, Space, Button, Input } from 'antd';
 import { MinusCircleOutlined, PlusOutlined } from '@ant-design/icons';
 import { FileUploader } from 'components/atoms/Input';
-
-function insertLineBreaks(inputText: string) {
-  const maxLineLength = 64;
-  const words = inputText.split(/\s+/); // Split the input into words
-
-  let currentLine = '';
-  let result = '';
-
-  for (let i = 0; i < words.length; i++) {
-    const word = words[i];
-
-    // Check if adding the word to the current line would exceed the limit
-    if (currentLine.length + word.length > maxLineLength) {
-      // If adding the word would exceed the limit, start a new line
-      result += currentLine.trim() + '\n';
-      currentLine = word + ' ';
-    } else {
-      // If adding the word would not exceed the limit, add it to the current line
-      currentLine += word + ' ';
-    }
-  }
-
-  // Add the last line (if any) to the result
-  result += currentLine.trim();
-
-  return result;
-}
+import { insertLineBreaks } from 'utils';
 
 const FileInputPairItem: React.FC<{ name: string }> = ({ name }) => {
   const [uploadedValue, setUploadedValue] = useState<
