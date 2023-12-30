aade125538be8d58667efd04f498d50a78ae61b4
Wolfgang <wleon@bakrypt.io>
2023-12-30 18:05:00 -0500

    wip: attrs reducer

--- src/components/composites/Asset/Card.tsx
+++ src/components/composites/Asset/Card.tsx
@@ -7,28 +7,6 @@ import { CarouselRef } from 'antd/es/carousel';
 
 const { Meta } = DSCard;
 
-const renderValue = (
-  value: string | number | [] | object,
-  reduced = '',
-  key?: string
-) => {
-  if (key) reduced += `${key} \n`;
-
-  if (Array.isArray(value)) {
-    for (let index = 0; index < value.length; index++) {
-      renderValue(value[index], reduced);
-    }
-  } else if (typeof value === 'object') {
-    for (const [q, entry] of Object.entries(value)) {
-      renderValue(entry, reduced, q);
-    }
-  } else {
-    reduced += `\t\t${value}`;
-    console.log(reduced);
-    return reduced;
-  }
-};
-
 const Card: React.FC<AssetProps> = ({
   name,
   description,
@@ -37,6 +15,59 @@ const Card: React.FC<AssetProps> = ({
   files,
 }) => {
   const ref = useRef<CarouselRef | null>(null);
+
+  // const [attrsValue, setAttrsValue] = useState<string>('');
+
+  // const renderValue = (
+  //   value: string | number | [] | object,
+  //   reduced = '',
+  //   key?: string
+  // ) => {
+  //   if (key) {
+  //     reduced += `${key} \n`;
+  //   }
+
+  //   if (Array.isArray(value)) {
+  //     for (let index = 0; index < value.length; index++) {
+  //       renderValue(value[index], reduced);
+  //     }
+  //   } else if (typeof value === 'object') {
+  //     for (const [q, entry] of Object.entries(value)) {
+  //       renderValue(entry, reduced, q);
+  //     }
+  //   } else {
+  //     reduced += `\t\t${value}`;
+  //   }
+  // };
+
+  const attrsReducer = (acc: string[], key: string) => {
+    if (!attrs) return acc;
+
+    if (key) {
+      acc.push(key);
+    }
+
+    // if (Array.isArray(value)) {
+    //   for (let index = 0; index < value.length; index++) {
+    //     renderValue(value[index], reduced);
+    //   }
+    // } else if (typeof value === 'object') {
+    //   for (const [q, entry] of Object.entries(value)) {
+    //     renderValue(entry, reduced, q);
+    //   }
+    // } else {
+    //   reduced += `\t\t${value}`;
+    // }
+
+    return acc;
+  };
+
+  // useEffect(() => {
+  //   if (attrs) {
+  //     const reduced = renderValue(attrs);
+  //     if (reduced) setAttrsValue(reduced);
+  //   }
+  // }, [renderValue, attrs]);
 
   const next = () => {
     ref?.current?.next();
@@ -62,7 +93,18 @@ const Card: React.FC<AssetProps> = ({
         {attrs && (
           <>
             <Divider orientation="left">Attributes</Divider>
-            {renderValue(attrs)}
+            {
+              <p>
+                {Object.keys(attrs)
+                  .reduce(attrsReducer, [])
+                  .map((line, index) => (
+                    <React.Fragment key={index}>
+                      {line}
+                      <br />
+                    </React.Fragment>
+                  ))}
+              </p>
+            }
           </>
         )}
 
@@ -91,7 +133,7 @@ const Card: React.FC<AssetProps> = ({
                 </>
               )}
 
-              <Carousel ref={ref} afterChange={(e) => console.log(e)}>
+              <Carousel ref={ref}>
                 {files.map((i, idx) => {
                   return (
                     <div key={`${idx}-${i.name}`}>
